8511348dcb1e29a6dc18c6a6b0b9ad15235c49b9
Иван Вякин <Beas_wow@mail.ru>
2025-02-21 15:33:33 +0300

    Рабочая версия

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Delegate_Exception.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,132 @@ namespace Delegate_Exception
     {
         static void Main(string[] args)
         {
+            // Задание 1, перехват исключений
+
+            int input = 0;
+            int[] arrayInput = {1, 3};
+            string str = null;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nВводим значение (при вводе 125 - выход из цикла первой задачи): ");
+                    if (int.TryParse(Console.ReadLine(), out input) is false)
+                    {
+                        throw new FormatException();
+                    }
+                    // Исключение если введено не число
+
+                    if (input == 125)
+                    {
+                        break;
+                    }
+                    // Выход из цикла первой части ДЗ
+
+                    if (input != 4 && input != 5)
+                    {
+                        if (input == 0)
+                        {
+                            throw new NullException();
+                        }
+                        throw new NonNeedNumberException();
+                    }
+                    // Исключения если введено не 4 или 5
+
+                    if (input == 4)
+                    {
+                        Console.WriteLine(str[2]);
+                    }
+                    // Демонстрация перехвата 1 системного исключения
+
+                    if (input == 5)
+                    { 
+                        Console.WriteLine(arrayInput[5]);
+                    }
+                    // Демонстрация перехвата 2 системного исключения
+                }
+                catch (Exception ex)
+                {
+                    if (ex is NonNeedNumberException)
+                    {
+                        Console.WriteLine("Введённое значение не равно 4 или 5");
+                    }
+
+                    if (ex is FormatException)
+                    {
+                        Console.WriteLine("Введено не целое число");
+                    }
+
+                    if (ex is NullException)
+                    {
+                        Console.WriteLine("Введён 0");
+                    }
+                    if (ex is IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Несуществующий элемент массива");
+                    }
+                    if (ex is NullReferenceException)
+                    {
+                        Console.WriteLine("Пустая строковая переменная");
+                    }
+                }
+            }
+
+            // Задание 2
+
+            List<string> surnames = new List<string>();
+            surnames.Add("Иванов");
+            surnames.Add("Бардов");
+            surnames.Add("Скопцов");
+            surnames.Add("Вишняков");
+            surnames.Add("Иванова");
+            // Создание списка-примера
+
+            Console.WriteLine("Не сортированный список:");
+            foreach (string s in surnames)
+            {
+                Console.WriteLine(s);
+            }
+            // Вывод необработанного списка
+
+            EventChoice eventChoice = new EventChoice();
+            eventChoice.ChoiceEvent += ChoicedSort;
+
+            try
+            {
+                eventChoice.CheckNum(surnames);
+            }
+            catch(Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine("Введено не число");
+                }
+                if (ex is NonNeedNumberException)
+                {
+                    Console.WriteLine("Введено неподходящее число");
+                }
+            }
+
+            Console.WriteLine("Сортированый список:");
+            foreach (string s in surnames)
+            {
+                Console.WriteLine(s);
+            }
+            // Вывод обработанного списка
         }
+
+        static void ChoicedSort(int number, List<string> surnames)
+        {
+            switch (number)
+            {
+                case 1: surnames.Sort();
+                    break;
+                case 2:
+                    surnames.Sort();surnames.Reverse();
+                    break;
+            }
+        }
+        // Метод-сортировщик
     }
 }
---
+++ Properties/EventChoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Exception.Properties
+{
+    class EventChoice
+    {
+        public delegate void EventChoiceDelegate(int number, List<string> list);  // Создание делегата для события
+        public event EventChoiceDelegate ChoiceEvent;  // Создание события
+
+        public void CheckNum(List<string> list)
+        {
+            Console.WriteLine("\nСортировка списка фамилий, введите значение" +
+                "\n1 - сортировка от А до Я\n2 - сортировка от Я до А");
+            
+            int number = 0;
+            if (Int32.TryParse(Console.ReadLine(), out number) == false)
+            {
+                throw new FormatException();
+            }
+            if (number != 1 && number != 2)
+            {
+                throw new NonNeedNumberException();
+            }
+
+            OnChoiceEvent(number, list);
+        }
+        // Метод для задачи способа сортировки
+
+        protected virtual void OnChoiceEvent(int number, List<string> list)
+        {
+            ChoiceEvent?.Invoke(number, list);
+        }
+    }
+}
